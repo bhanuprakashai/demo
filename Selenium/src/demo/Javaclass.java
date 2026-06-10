b6277a5ebaed8fa9ec3db90b3da9c6ee3bf2a054
bhanuprakashai <bhanuprakashskht@gmail.com>
2026-06-10 14:29:02 +0530

    git push

---
+++ Selenium/src/demo/Javaclass.java
@@ -0,0 +1,20 @@
+package demo;
+public class Javaclass {
+public static void main(String[] args) {
+System.out.println("HELLO WORLD  This is bhanu praksh kalahsti");
+System.out.println(7+9);
+System.out.println("7=9");
+System.out.print("GITHUB");
+System.out.println("postman");
+System.out.print("JSON");
+// techlearn
+
+
+ 
+ 
+
+
+
+	}
+
+}
---
+++ Selenium/src/demo/Varaiables.java
@@ -0,0 +1,16 @@
+package demo;
+
+public class Varaiables {
+
+	public static void main(String[] args) {
+		String firstName = "Bhanu";
+		String lastName = "Prakash";
+		
+		         
+		
+				
+				
+
+	}
+
+}
